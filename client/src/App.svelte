b8fa480f6e8615f71c9ece3bd4a9c3eebebe6424
Chris Ye <chye0001@kea.stud.dk>
2025-04-15 13:23:31 +0200

    refactored signin/signup/signout btn moved this fucntionality into componenet: AuthButton.svelte

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,28 +1,10 @@
 <script>
-  import { Router, Link, Route, navigate } from 'svelte-routing';
+  import { Router, Link, Route } from 'svelte-routing';
   import Authentication from './pages/Authentication/Authentication.svelte';
   import Home from './pages/Home/Home.svelte';
   import PetJournal from './pages/PetJournal/PetJournal.svelte';
   import { authStore } from './stores/authStore.js';
-  import { authCardState } from './stores/authCardStateStore.js';
-
-  function onSignIn() {
-    console.log("signin");
-    
-    authCardState.flipToSignIn();
-    navigate("/authenticate", { replace: true });
-  }
-
-  function onSignUp() {
-    console.log("signup");
-    
-    authCardState.flipToSignUp();
-    navigate("/authenticate", { replace: true });
-  }
-
-  function signOut() {
-    authStore.signOut();
-  }
+  import AuthButton from './components/Authentication/AuthButton/AuthButton.svelte';
 
   const url = "";
 </script>
@@ -31,16 +13,7 @@
   <nav>
     <Link to="/">Home</Link>
     <Link to="/petjournal">Pet Journal</Link>
-    <div>
-      {#if !$authStore.isSignedIn}
-        <button onclick={onSignIn}>Sign in</button>
-        <button onclick={onSignUp}>Sign up</button>
-      
-      {:else}
-        <button onclick={signOut}>Sign out</button>
-    
-      {/if}
-    </div>
+    <AuthButton/>
   
   </nav>
 
---
+++ client/src/components/Authentication/AuthButton/AuthButton.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { navigate } from "svelte-routing";
+  import { authCardState } from "../../../stores/authCardStateStore.js";
+  import { authStore } from "../../../stores/authStore.js";
+
+  function onSignIn() {
+    authCardState.flipToSignIn();
+    navigate("/authenticate", { replace: true });
+  }
+
+  function onSignUp() {
+    authCardState.flipToSignUp();
+    navigate("/authenticate", { replace: true });
+  }
+
+  function signOut() {
+    authStore.signOut();
+  }
+</script>
+
+<div>
+  {#if !$authStore.isSignedIn}
+    <button onclick={onSignIn}>Sign in</button>
+    <button onclick={onSignUp}>Sign up</button>
+
+  {:else}
+    <button onclick={signOut}>Sign out</button>
+    
+  {/if}
+</div>
